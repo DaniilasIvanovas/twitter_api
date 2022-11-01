45ad1bd01d3b5ac949c3eeea53e66112834a4265
Daniilas <47268986+DaniilasIvanovas@users.noreply.github.com>
2022-11-01 17:41:10 +0200

    Add files via upload

---
+++ create_sheet.py
@@ -0,0 +1,16 @@
+import openpyxl
+
+
+def create_sheet():
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.title = 'Tweets'
+    parameters = ['Tweet Id', 'Language', 'Text lenght', 'Hastags', 'Symbols', 'Mentions', 'Url']
+    col = 1
+    for parameter in parameters:
+        cell = sheet.cell(row=1, column=col)
+        cell.value = parameter
+        col += 1
+    wb.save('Testas.xlsx')
+
+create_sheet()
